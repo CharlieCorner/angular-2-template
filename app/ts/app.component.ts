2f3311c679a3860000018429cd0f83c84a133be7
Charles Corner <ccorner.dev@gmail.com>
2016-04-05 00:12:45 -0400

    Add all of the files created up to Tutorial 9

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,23 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from './playlist.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+	mainHeading = Config.MAIN_HEADING;
+	videos: Array<Video>;
+
+	constructor(){
+		this.videos = [
+			new Video(1, "MarIO", "qv6UVOQ0F44", "I like NN!"),
+			new Video(2, "Marching cats!", "lAIGb1lfpBw", "I like cats!"),
+			new Video(3, "PPG", "GbvC9Xtpm_8", "Sugar Spice and everything nice!")
+		]
+	}
+}
